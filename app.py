0260254412239502fd7a150d0e041893f9dff866
nathanael <n.bellpactat@gmail.com>
2021-07-27 13:17:33 -0400

    Add fields to Artist model, remove nullable=False attributes, upgrade db

--- app.py
+++ app.py
@@ -31,15 +31,15 @@ class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    address = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     website = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
-    seeking_talent = db.Column(db.Boolean, nullable=False)
-    seeking_description = db.Column(db.String(500), nullable=True)
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
@@ -55,6 +55,10 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(500))
+
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
