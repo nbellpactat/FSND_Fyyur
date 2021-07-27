058f0300ec3729667f8793568a38d13f09753131
nathanael <n.bellpactat@gmail.com>
2021-07-27 13:31:15 -0400

    Refactor whitespace

--- app.py
+++ app.py
@@ -63,15 +63,15 @@ class Artist(db.Model):
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 class Show(db.Model):
-  __tablename__ = 'Show'
+    __tablename__ = 'Show'
 
-  id = db.Column(db.Integer, primary_key=True)
-  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'))
-  venue_name = db.Column(db.String)
-  artist_id = db.Column(db.Integer)
-  artist_name = db.Column(db.String)
-  artist_image_link = db.Column(db.String(500))
-  start_time = db.Column(db.DateTime)
+    id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'))
+    venue_name = db.Column(db.String)
+    artist_id = db.Column(db.Integer)
+    artist_name = db.Column(db.String)
+    artist_image_link = db.Column(db.String(500))
+    start_time = db.Column(db.DateTime)
 
 
 #----------------------------------------------------------------------------#
