068b6364572b959fe632149eb3f0779614c6ae6f
nathanael <n.bellpactat@gmail.com>
2021-07-28 21:41:45 -0400

    Fix the format_datetime function to accept values already as datetime obj instead of str

--- app.py
+++ app.py
@@ -91,7 +91,10 @@ class Show(db.Model):
 # Filters.
 # ----------------------------------------------------------------------------#
 def format_datetime(value, format='medium'):
-    date = dateutil.parser.parse(value)
+    if isinstance(value, str):
+        date = dateutil.parser.parse(value)
+    else:
+        date = value
     if format == 'full':
         format = "EEEE MMMM, d, y 'at' h:mma"
     elif format == 'medium':
