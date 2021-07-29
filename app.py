5df480f16529512c4127da101b10802f5ad732fc
nathanael <n.bellpactat@gmail.com>
2021-07-29 11:02:23 -0400

    Complete the /artists endpoint

--- app.py
+++ app.py
@@ -435,18 +435,26 @@ def delete_venue(venue_id):
 #  ----------------------------------------------------------------
 @app.route('/artists')
 def artists():
-    # TODO: replace with real data returned from querying the database
-    data = [{
-        "id": 4,
-        "name": "Guns N Petals",
-    }, {
-        "id": 5,
-        "name": "Matt Quevedo",
-    }, {
-        "id": 6,
-        "name": "The Wild Sax Band",
-    }]
-    return render_template('pages/artists.html', artists=data)
+    error = False
+    data = []
+    try:
+        artists_list = Artist().query.all()
+        for artist in artists_list:
+            data.append(
+                {
+                    "id": artist.id,
+                    "name": artist.name
+                }
+            )
+    except:
+        error = True
+        error_line_number()
+    finally:
+        db.session.close()
+    if error:
+        abort(500)
+    else:
+        return render_template('pages/artists.html', artists=data)
 
 
 @app.route('/artists/search', methods=['POST'])
