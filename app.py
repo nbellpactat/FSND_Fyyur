a217ee5f52778f336ffb6dc0879b18a303d6774b
nathanael <n.bellpactat@gmail.com>
2021-08-01 14:30:03 -0600

    Refactor flask imports for pep8

--- app.py
+++ app.py
@@ -9,7 +9,17 @@ from collections import defaultdict
 
 import dateutil.parser
 import babel
-from flask import Flask, render_template, request, Response, flash, redirect, url_for, jsonify, abort
+from flask import (
+    Flask,
+    render_template,
+    request,
+    Response,
+    flash,
+    redirect,
+    url_for,
+    jsonify,
+    abort
+)
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
